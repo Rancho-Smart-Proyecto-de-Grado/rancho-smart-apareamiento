f3a2aa27c1500a1fe6b98c39fa7e9f5f684b232f
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-12 04:47:11 -0500

    :sparkles: feat: Creación de la Aplicación SpringBoot.

---
+++ src/main/java/com/rancho_smart/apareamiento_composer/ApareamientoComposerApplication.java
@@ -0,0 +1,13 @@
+package com.rancho_smart.apareamiento_composer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApareamientoComposerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApareamientoComposerApplication.class, args);
+	}
+
+}
